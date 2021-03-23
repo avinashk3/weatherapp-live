1795288073deb3be2a9511e307e60152ec0a3ac6
Avinash Kumar <officialaviid@gmail.com>
2021-03-23 21:17:47 +0530

    initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, HttpResponse
+from dashboard.forms import CityForm
+from .models import City
+from .helper import getweatherdata
+
+# Create your views here.
+
+
+def home(request):
+    form = CityForm()
+    context = {}
+    if request.method == 'POST':
+        print("post")
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+            city_name = form.cleaned_data.get("city_name")
+            print(city_name)
+            weather_data = getweatherdata(city_name)
+            
+
+    elif request.method == "GET":
+        city_name = City.objects.latest("date_added").city_name
+        weather_data = getweatherdata(city_name)    
+    
+    context['weather_data'] = weather_data
+    context['form'] = form
+
+
+    return render(request, 'home.html', context)
+
+
+def history(request):
+    cities = City.objects.all().order_by('-date_added')[:5]
+    print(cities)
+    weather = []
+    for city in cities:
+        city_name = city.city_name
+        weather.append(getweatherdata(city_name))
+    
+    context = {
+        'weather' : weather,
+    }
+    return render(request, 'history.html', context)
